af49cd2afd2e8a5e7edc8dab2ed4fa2326341d79
Dan Aukes <danaukes@gmail.com>
2021-12-07 09:01:55 -0700

    updated pp-beamer

--- python/pandoc_plus/pp-beamer.py
+++ python/pandoc_plus/pp-beamer.py
@@ -23,41 +23,53 @@ slideous_path = 'file:///' + '/'.join([support_path,'slideous'])
 s5_path = 'file:///' + '/'.join([support_path,'s5-11','ui','default'])
 revealjs_path = 'file:///' + '/'.join([support_path,'reveal.js'])
 
-def process_file(input_file_exp,output_extension):
+def process_file(path,output_extension):
 
 
     output_extension = output_extension.strip()
-    input_file_exp = input_file_exp.strip()
+    input_files = []
+    for item in path:
+        input_file_exp = item.strip()
 
-    print('input: ',input_file_exp,', output extension: ', output_extension)
+        print('input: ',input_file_exp,', output extension: ', output_extension)
 
-    input_files = glob.glob(input_file_exp)
-    for input_file in input_files:
+        input_files.extend(glob.glob(input_file_exp))
+        # for input_file in input_files:
         
-        input_name,input_extension = os.path.splitext(input_file)
+    input_file_string = '"'+'" "'.join(input_files)+'"'
+    input_name,dummy = os.path.splitext(input_files[0])
+    
+    if output_extension in ['pdf','tex']:
+        s='pandoc '+project_path+'/bat/beamer_default.yaml -V titlegraphic="'+support_path+'/fulton.png'+'" -s -t beamer --pdf-engine=xelatex --slide-level=2 -o "'+input_name+'.'+output_extension+'" '+input_file_string 
         
-        if output_extension in ['pdf','tex']:
-            s='pandoc '+project_path+'/bat/beamer_default.yaml -V titlegraphic="'+support_path+'/fulton.png'+'" -s -t beamer --pdf-engine=xelatex --slide-level=2 "'+input_file+'" -o "'+input_name+'.'+output_extension+'"'
-            
-        elif output_extension in ['slidy','slideous','s5','dzslides','revealjs']:
-            s='pandoc -s -t '+output_extension+' --slide-level=2 -V slidy-url="'+slidy_path+'" -V slideous-url="'+slideous_path+'" -V s5-url="'+s5_path+'" -V revealjs-url="'+revealjs_path+'" "'+input_file+'" -o "'+input_name+'.html"'
+    elif output_extension in ['slidy','slideous','s5','dzslides','revealjs']:
+        s='pandoc -s -t '+output_extension+' --slide-level=2 -V slidy-url="'+slidy_path+'" -V slideous-url="'+slideous_path+'" -V s5-url="'+s5_path+'" -V revealjs-url="'+revealjs_path+'" -o "'+input_name+'.html" '+input_file_string 
 
-        elif output_extension =='pptx':
-            s='pandoc -s --slide-level=2 "'+input_file+'" -o "'+input_name+'.'+ output_extension+'"'
-        
-        print(s)
-        subprocess.run(s,shell = True,check = True,stdout=subprocess.PIPE,stderr = subprocess.STDOUT)        
+    elif output_extension =='pptx':
+        s='pandoc -s --slide-level=2 -o "'+input_name+'.'+ output_extension+'" '+input_file_string 
+    
+    print(s)
+    subprocess.run(s,shell = True,check = True,stdout=subprocess.PIPE,stderr = subprocess.STDOUT)        
     
 if __name__=='__main__':
-    # import argparse
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-i',dest='input',default = None)
-    # parser.add_argument('-o',dest='output_extension',default = None)
-    # parser.add_argument('-t',dest='template',default = None)
-    # args = parser.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',metavar='path',type=str,help='path', default = None,nargs='+')
 
-    input_file = sys.argv[1]
-    output_extension = sys.argv[2]
+    # parser.add_argument('-i',dest='input',default = None)
+    parser.add_argument('-o',dest='output_extension',default = None)
+    parser.add_argument('-d','--debug',dest='debug',action='store_true', default = False)
+    # parser.add_argument('-t',dest='template',default = None)
+    args = parser.parse_args()
+
+    # input_file = sys.argv[1]
+    # output_extension = sys.argv[2]
     
-    process_file(input_file,output_extension)
+    path = [os.path.normpath(os.path.expanduser(item)) for item in args.path]
+    
+    if args.debug:
+        print('path: ',path)
+        print('output_extension: ',args.output_extension)
+
+    process_file(path,args.output_extension)
 
