97d556afdf53f5574edd1beb59e460cb70c08c1c
Dan Aukes <danaukes@gmail.com>
2022-06-08 15:16:19 -0700

    updated

--- python/pandoc_plus/pandoc-plus.py
+++ python/pandoc_plus/pandoc-plus.py
@@ -19,7 +19,7 @@ project_path = initfile.split(os.path.sep)
 project_path = os.path.sep.join(project_path[:-3])
 pandoc_dir = os.path.join(project_path,'pandoc')
 
-def process_file(path,output_extension,template):
+def process_file(path,output_extension,template,merge):
 
 
     output_extension = output_extension.strip()
@@ -33,41 +33,50 @@ def process_file(path,output_extension,template):
         # for input_file in input_files:
 
     input_file_string = '"'+'" "'.join(input_files)+'"'
-    input_name,dummy = os.path.splitext(input_files[0])
+    input_name,input_extension = os.path.splitext(input_files[0])
 
     template = template.strip()
 
     print('input: ',input_file_exp,', output extension: ', output_extension,', template: ',template)
 
     # input_files = glob.glob(input_file_exp)
-    for input_file in input_files:
-        
-        input_name,input_extension = os.path.splitext(input_file)
-        
-        
-        input_extension = input_extension[1:].lower()
-        if  input_extension == "docx":
-            extract_dir = input_name+'_media'
-            os.mkdir(extract_dir)
-            extractstring = '--extract-media="./'+extract_dir+'" '
-        else:
-            extractstring = ''
-        #print(input_extension,extractstring)
     
-        if output_extension == 'pdf':
-            s='pandoc "'+input_file+'" -s -t latex+smart --citeproc --data-dir='+pandoc_dir+' --template='+template+'.tex --pdf-engine=xelatex '+extractstring+'--wrap=none --reference-links -o "'+input_name+'.'+ output_extension+'"'
-        elif output_extension == 'docx':
-            s='pandoc "'+input_file+'" -s --reference-doc='+pandoc_dir+'/'+template+'.docx -o "'+input_name+'.'+output_extension+'"'
-        elif output_extension == 'odt':
-            s='pandoc "'+input_file+'" -s -o "'+input_name+'.'+output_extension+'"'
-        elif output_extension == 'tex':
-            s='pandoc "'+input_file+'" -s -t latex+smart --natbib --data-dir='+pandoc_dir+' --template='+template+'.tex --pdf-engine=xelatex '+extractstring+'--wrap=none --reference-links -o "'+input_name+'.'+ output_extension+'"'
-        elif output_extension =='md':
-            s='pandoc "'+input_file+'" -s --wrap=none '+extractstring+' --markdown-headings=atx -t markdown-raw_html-bracketed_spans-native_spans-native_divs+fenced_divs -o "'+input_name+'.'+ output_extension+'"'
-            #s='pandoc "'+input_file+'" -s --wrap=none '+extractstring+' --markdown-headings=atx -t markdown-raw_html-bracketed_spans-native_spans-native_divs+fenced_divs-grid_tables-multiline_tables-simple_tables+pipe_tables -o "'+input_name+'.'+ output_extension+'"'
-        
-        print(s)
-        result = subprocess.run(s,shell = True,check = True,capture_output=True)
+    #print(input_extension,extractstring)
+    
+    extractstring = ''
+    #print(input_extension,extractstring)
+    if merge:
+        process_internal(input_file_string,input_name,input_extension,output_extension,template)
+    else:
+        for input_file in input_files:
+            input_name,input_extension = os.path.splitext(input_file)
+            process_internal(input_file,input_name,input_extension,output_extension,template)
+
+def process_internal(input_file_string,input_name,input_extension,output_extension,template):
+
+    input_extension = input_extension[1:].lower()
+    if  input_extension == "docx":
+        extract_dir = input_name+'_media'
+        os.mkdir(extract_dir)
+        extractstring = '--extract-media="./'+extract_dir+'" '
+    else:
+        extractstring = ''
+
+
+    if output_extension == 'pdf':
+        s='pandoc -s -t latex+smart --citeproc --data-dir='+pandoc_dir+' --template='+template+'.tex --pdf-engine=xelatex '+extractstring+'--wrap=none --reference-links -o "'+input_name+'.'+ output_extension+'" '+input_file_string
+    elif output_extension == 'docx':
+        s='pandoc -s --reference-doc='+pandoc_dir+'/'+template+'.docx -o "'+input_name+'.'+output_extension+'" '+input_file_string
+    elif output_extension == 'odt':
+        s='pandoc -s -o "'+input_name+'.'+output_extension+'" '+input_file_string
+    elif output_extension == 'tex':
+        s='pandoc -s -t latex+smart --natbib --data-dir='+pandoc_dir+' --template='+template+'.tex --pdf-engine=xelatex '+extractstring+'--wrap=none --reference-links -o "'+input_name+'.'+ output_extension+'" '+input_file_string
+    elif output_extension =='md':
+        s='pandoc -s --wrap=none '+extractstring+' --markdown-headings=atx -t markdown-raw_html-bracketed_spans-native_spans-native_divs+fenced_divs -o "'+input_name+'.'+ output_extension+'" '+input_file_string
+        #s='pandoc -s --wrap=none '+extractstring+' --markdown-headings=atx -t markdown-raw_html-bracketed_spans-native_spans-native_divs+fenced_divs-grid_tables-multiline_tables-simple_tables+pipe_tables -o "'+input_name+'.'+ output_extension+'" '+input_file_string
+    
+    print(s)
+    result = subprocess.run(s,shell = True,check = True,capture_output=True)
     return 
         
 if __name__=='__main__':
@@ -77,6 +86,7 @@ if __name__=='__main__':
     parser.add_argument('path',metavar='path',type=str,help='path', default = None,nargs='+')
     parser.add_argument('-o',dest='output_extension',default = None)
     parser.add_argument('-t',dest='template',default = None)
+    parser.add_argument('-m','--merge',dest='merge',action='store_true', default = False)
     args = parser.parse_args()
     
     
@@ -93,5 +103,5 @@ if __name__=='__main__':
     # output_extension = sys.argv[2]
     # template = sys.argv[3]
 
-    result = process_file(path,args.output_extension,args.template)
+    result = process_file(path,args.output_extension,args.template,args.merge)
 
